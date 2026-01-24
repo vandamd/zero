bf1c292839ff0a3575d6796067e88a781ec4e61c
vandamd <vandamdinh71@gmail.com>
2026-01-24 23:43:00 +0000

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.vandam.zero"
         minSdk = 29
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.2.6"
+        versionCode = 10
+        versionName = "1.2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
